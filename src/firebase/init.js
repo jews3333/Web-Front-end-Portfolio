2c3f091ada4003aa435e69408406d58aee90d5c4
unknown <bu8886679@BSVDI.COM>
2020-01-10 18:00:23 +0900

    데이터베이스를 파이어스토어로 변경중

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,6 +3,7 @@ import Config from './config';
 
 firebase.initializeApp(Config);
 
+export const firestore = firebase.firestore();
 export const database = firebase.database();
 export const auth = firebase.auth;
 export const provider = new firebase.auth.GoogleAuthProvider();
